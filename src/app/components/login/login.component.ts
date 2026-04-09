31921d8278ab44991bc4f356a21c208fda791ea3
Jake Garland <jake@richmondapps.com>
2026-04-09 08:31:39 -0700

    feat(auth): remove deprecated email magic link flow due to security quarantine

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,30 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
   private authService = inject(AuthService);
-  email = '';
-  isSending = false;
 
   ngOnInit() {
-    this.authService.confirmMagicLink(window.location.href);
   }
 
   loginWithGoogle() {
     this.authService.loginWithGoogle();
-  }
-
-  async sendMagicLink() {
-    if (!this.email) {
-      alert("Please enter your email address first");
-      return;
-    }
-    this.isSending = true;
-    await this.authService.sendMagicLink(this.email);
-    this.isSending = false;
   }
 }
